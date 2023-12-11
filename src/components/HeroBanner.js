4b9e4d2fdff97d7b830b1983ae66aea32d96b9b8
andrewpersaud1 <andrewpersaud022@gmail.com>
2023-12-11 14:42:21 -0500

    finished with home page

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -12,13 +12,23 @@ const HeroBanner = () => {
         FitnessClub
       </Typography>
       <Typography fontWeight={700} 
-      sx={{fontSize: {lg:'44px', xs:'40px'}}}>
+      sx={{fontSize: {lg:'44px', xs:'40px'}}}
+      mb="23px" mt="30px">
         Sweat, Smile <br/> and Repeat
       </Typography>
-      <Typography fontSize='22px' lineHeight='35px' mb={3}>
+      <Typography fontSize='22px' lineHeight='35px' mb={4}>
         Check out the most effective exercises
       </Typography>
-      <Button variant="contained" color="error" href="#exercises">Explore Exercises</Button>
+      <Button variant="contained" color="error" href="#exercises" sx={{backgroundColor:'#ff2625', padding:'10px'}}>Explore Exercises</Button>
+      <Typography 
+      fontWeight={600} 
+      color="#ff2625" 
+      sx={{opacity:0.1, 
+      display:{lg: 'block', sm:'none'}}}
+      fontSize="200px"
+      >
+        Exercise
+      </Typography>
       <img src={HeroBannerImage} alt="hero-banner" className="hero-banner-img"/>
     </Box>
   )
